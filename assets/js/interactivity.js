7eb19c0280995e18cb433e6cad44d0e24939ef2a
Pedro Barros <pedrobarros@Pedros-MacBook-Pro.local>
2025-06-19 17:08:52 -0300

    fase2 es

---
+++ assets/js/interactivity.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Bem-vindo ao Mini Mercado XYZ!');
+    setInterval(() => {
+        console.log('Confira nossas promoções!');
+    }, 5000);
+});
